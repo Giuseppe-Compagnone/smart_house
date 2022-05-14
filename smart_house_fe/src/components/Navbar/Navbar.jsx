4aea9ce17da21aef7b6d63717af3ec40da0dbdb1
Giuseppe Compagnone <94106986+Giuseppe-Compagnone@users.noreply.github.com>
2022-05-14 15:40:40 +0200

    Navbar end

--- smart_house_fe/src/components/Navbar/Navbar.jsx
+++ smart_house_fe/src/components/Navbar/Navbar.jsx
@@ -1,13 +1,27 @@
 import "./Navbar.css";
 
+const handleClick = () => {
+    const menu = document.querySelector("#list");
+    if(menu.classList.contains("hidden")){
+        menu.classList.remove("hidden");
+    }
+}
+
+const handleClose = () => {
+    const menu = document.querySelector("#list");
+    if(!menu.classList.contains("hidden")){
+        menu.classList.add("hidden");
+    }
+}
+
 const Navbar = () => {
     return(
         <nav>
             <h1 id="title" className="gradient-text">Smart House</h1>
-            <i class="fa-solid fa-bars"></i>
+            <i className="fa-solid fa-bars" onClick={()=>handleClick()}></i>
             <div id="menu">
-                <ul>
-                    <li><i class="fa-solid fa-xmark" id="close"></i></li>
+                <ul id="list" className="hidden">
+                    <li id="icon"><i className="fa-solid fa-xmark" id="close" onClick={()=>handleClose()}></i></li>
                     <li><a href="#">ROOMS</a></li>
                     <span></span>
                     <li><a href="#">INFO</a></li>
