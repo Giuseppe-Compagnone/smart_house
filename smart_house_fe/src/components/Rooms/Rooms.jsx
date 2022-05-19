03c070f84361ae0eda38eb4655efd1b1ac390a7f
Giuseppe Compagnone <94106986+Giuseppe-Compagnone@users.noreply.github.com>
2022-05-19 09:49:16 +0200

    Rooms update

--- smart_house_fe/src/components/Rooms/Rooms.jsx
+++ smart_house_fe/src/components/Rooms/Rooms.jsx
@@ -6,11 +6,11 @@ const Rooms = () =>{
             <main id="houseLink">
                 <div id="house" className="room">
                     <div id="rooms">
-                        <div id="garage" className="room"></div>
-                        <div id="bedroom" className="room"></div>
-                        <div id="living" className="room"></div>
-                        <div id="bathroom" className="room"></div>
-                        <div id="garden" className="room"></div>
+                        <div id="garage" className="room">Garage</div>
+                        <div id="bedroom" className="room">Bedroom</div>
+                        <div id="living" className="room">Living</div>
+                        <div id="bathroom" className="room">Bathroom</div>
+                        <div id="garden" className="room">Garden</div>
                     </div>
                 </div>
             </main>
