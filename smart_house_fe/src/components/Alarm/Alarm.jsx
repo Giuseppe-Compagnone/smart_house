9671c9a33b380e60b793e0bf4958aae4ab12a021
Giuseppe Compagnone <94106986+Giuseppe-Compagnone@users.noreply.github.com>
2022-05-23 13:43:21 +0200

    alarm finished

--- smart_house_fe/src/components/Alarm/Alarm.jsx
+++ smart_house_fe/src/components/Alarm/Alarm.jsx
@@ -1,36 +1,44 @@
 import "./Alarm.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faLock ,faLockOpen} from "@fortawesome/free-solid-svg-icons";
+import { faLock, faLockOpen } from "@fortawesome/free-solid-svg-icons";
+import { useRef, useEffect, useState } from "react";
 
 const Alarm = () => {
-
-    const close = <FontAwesomeIcon icon={faLock} />;
-    const open = <FontAwesomeIcon icon={faLockOpen} />
-
-    // let lock = document.querySelector(".toggler");
-
-    // console.log(lock);
-
-    // const condition = lock.classList.contains("active"); 
+  const close = <FontAwesomeIcon icon={faLock} className="lock" />;
+  const open = <FontAwesomeIcon icon={faLockOpen} className="lock" />;
+  const [condition, setCondition] = useState(false);
+  let lock = useRef();
 
 
-    const handleToggle = (e) =>{
-        e.target.classList.toggle("active");
-    }
 
-    return(
-        <>
-            <section className="alarm">
-                <h1>Alarm</h1>
-                <div className="alarm-options">
-                    <div id="alarm-container" className="room-style"><p>alarm {}</p></div>
-                    <div id="hold" className="toggler" onClick={(e)=>{handleToggle(e)}}>
-                        <i className="trigger" ></i>
-                    </div>                    
-                </div>
-            </section>
-        </>
-    );
+  const handleToggle = (e) => {
+    e.target.classList.toggle("active");
+    setCondition(lock.current.classList.contains("active"));
+
+  };
+
+  return (
+    <>
+      <section className="alarm">
+        <h1>Alarm</h1>
+        <div className="alarm-options">
+          <div id="alarm-container" className="room-style">
+            <p>alarm {condition ? close : open}</p>
+          </div>
+          <div
+            id="hold"
+            className="toggler"
+            ref={lock}
+            onClick={(e) => {
+              handleToggle(e);
+            }}
+          >
+            <i className="trigger" />
+          </div>
+        </div>
+      </section>
+    </>
+  );
 };
 
 export default Alarm;
