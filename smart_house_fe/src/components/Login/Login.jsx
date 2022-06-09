c5ac6fd65dd82da5807230f44ba8f1e112d5932a
Giuseppe Compagnone <compagnonegiuseppe04@gmail.com>
2022-06-09 22:05:50 +0200

    login styling

--- smart_house_fe/src/components/Login/Login.jsx
+++ smart_house_fe/src/components/Login/Login.jsx
@@ -5,19 +5,20 @@ import SignIn from "./../SignIn/SignIn";
 const Login = (props) =>{
 
     const [ user , setUser ] = useState({email:"",psw:""});
+    const [ error , setError] = useState("");
 
     return(
         <>
         <h1>Welcome, please Log in</h1>
-        <p id="error"></p>
+        <p className="error">{error}</p>
         <form>
-            <label htmlFor="email"></label>
+            <label htmlFor="email">Email</label>
             <input className="input" type="email" name="email" placeholder="Your Email" onChange={(e)=>{setUser({...user,email:e.target.value})}} value={user.email} required />
-            <label htmlFor=""></label>
+            <label htmlFor="">Password</label>
             <input className="input" type="password" name="psw" placeholder="Your Password" onChange={(e)=>{setUser({...user,psw:e.target.value})}} value={user.psw} required />
             <input type="submit" value="Submit"/>
         </form>
-        <a href="#" id="sign-in" onClick={props.set(false)}>Do not have an account? Sign in</a>
+        <a href="#" id="sign-in" onClick={()=>props.set(false)}>Do not have an account? Sign in</a>
     </>
     );    
 } 
--- smart_house_fe/src/components/SignIn/SignIn.jsx
+++ smart_house_fe/src/components/SignIn/SignIn.jsx
@@ -1,10 +1,25 @@
 import "./SignIn.css";
+import { useState } from "react";
 
 const SignIn = (props) =>{
+
+const [ sign , setSign] = useState({email:"",psw:"",confirm:""});
+const [ error , setError] = useState("");
+
     return(
         <>
-            BEHLLLLAAA RAGAAAAA
-            <a href="#" id="log-in" >Do have an account? Log in</a>
+            <h1>Welcome, please Sign in</h1>
+            <p className="error">{error}</p>
+            <form>
+                <label htmlFor="email">Email</label>
+                <input className="input" type="email" name="email" placeholder="Your Email" onChange={(e)=>{setSign({...sign,email:e.target.value})}} value={sign.email} required />
+                <label htmlFor="">Password</label>
+                <input className="input" type="password" name="psw" placeholder="Your Password" onChange={(e)=>{setSign({...sign,psw:e.target.value})}} value={sign.psw} required />
+                <label htmlFor="confirm">Confirm Password</label>
+                <input className="input" type="password" name="confirm" placeholder="Confirm your Password" onChange={(e)=>{setSign({...sign,confirm:e.target.value})}} value={sign.confirm} required />
+                <input type="submit" value="Submit"/>
+            </form>
+            <a href="#" id="log-in" onClick={()=>props.set(true)}>Do have an account? Log in</a>
         </>
     )
 }
